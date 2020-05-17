2121fedc237c8cfe1c677a78af808d48b74af0d4
Lehel44 <Lehel44@hotmail.com>
2020-05-18 01:01:35 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Flights from "./components/table";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import MainHeroImage from "./components/MainHeroImage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const heroContent = {
+    title: 'Heathrow and Luton flight information',
+    description:
+        "The table below contains live flight information from Heathrow and Luton airports in London. You can find the arrivals to Heathrow and " +
+        "both the arrivals and departures at Luton airport.",
+    image: 'https://images.unsplash.com/photo-1464037866556-6812c9d1c72e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+};
+
+class App extends Component {
+    state = {
+        flight_data: []
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="lg">
+                    <main>
+                        <MainHeroImage post={heroContent} />
+                    </main>
+                    <Flights flights={this.state.flight_data} />
+                </Container>
+            </React.Fragment>
+        );
+    }
+
+    componentDidMount() {
+        const url = 'http://localhost:3001/api/v1/flights'
+        fetch(url)
+            .then(res => res.json())
+            .then((res) => {
+                this.setState({flight_data: res})
+            })
+            .catch(console.log)
+    }
 }
 
 export default App;
