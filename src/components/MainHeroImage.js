a5a16bf7f01b9406a5b7efa4d6b05a8aed2ef815
Lehel44 <Lehel44@hotmail.com>
2020-05-18 01:59:21 +0200

    Removed unused import.

--- src/components/MainHeroImage.js
+++ src/components/MainHeroImage.js
@@ -4,7 +4,6 @@ import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
     mainFeaturedPost: {
@@ -42,7 +41,7 @@ export default function MainHeroImage(props) {
     return (
         <Paper className={classes.mainFeaturedPost} style={{backgroundImage: `url(${post.image})`}}>
             {/* Increase the priority of the hero background image */}
-            {<img style={{display: 'none'}} src={post.image}/>}
+            {<img style={{display: 'none'}} src={post.image} alt="airport"/>}
             <div className={classes.overlay}/>
             <Grid container>
                 <Grid item md={6}>
